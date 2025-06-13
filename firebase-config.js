291de9ece127876ccaa6b28f4e0359a36fb30158
pranjalnamey <48804873+pranjalnamey@users.noreply.github.com>
2025-06-13 20:06:58 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByurVc4iWvu1CsoeLSyeErKszQhafCk0w",
+  authDomain: "invoice-tool-access.firebaseapp.com",
+  projectId: "invoice-tool-access",
+  storageBucket: "invoice-tool-access.firebasestorage.app",
+  messagingSenderId: "61154350236",
+  appId: "1:61154350236:web:4d5e2b4b748826ef250959"
+};
